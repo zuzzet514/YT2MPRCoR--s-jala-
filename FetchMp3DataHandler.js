8ba83872af5b2d98e931c3acb47d79ef2f98301d
Zuzzet Suarez <zuzzet.hs14@gmail.com>
2024-11-29 15:55:29 -0600

    .

--- FetchMp3DataHandler.js
+++ FetchMp3DataHandler.js
@@ -1,6 +1,8 @@
 const Handler = require("./Handler");
 const fetch = require("node-fetch");
 
+// https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}
+
 class FetchMp3DataHandler {
 
 }
--- ParseYoutubeLinkHandler.js
+++ ParseYoutubeLinkHandler.js
@@ -1,7 +1,19 @@
 const Handler = require("./Handler");
 
 class ParseYoutubeLinkHandler {
+    async handle(input) {
 
+        const videoIdMatch = input.match(/(?:\?v=|&v=|youtu\.be\/|youtube\.com\/embed\/)([^&\s]+)/);
+        const videoId = videoIdMatch ? videoIdMatch[1] : input.trim();
+
+        if (!videoId || videoId === "") {
+            console.error("Error: Invalid input. Please enter a valid YT link or video ID")
+            return;
+        }
+
+        console.log(`Extracted video ID ${videoId}`);
+        return super.handle(videoId);
+    }
 }
 
 module.exports = { ParseYoutubeLinkHandler };
